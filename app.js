6066bd39fe6c39b8036782602f98920859cbd69f
AlisherAbsaitov <alisher24032002@gmail.com>
2024-09-25 15:02:32 +0500

    add

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import { fileURLToPath } from "url";
 import path from "path";
+import { log } from "console";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+console.log(__dirname);
 
 app.get("/", (req, res) => {
   res.send("Welcome to server");
@@ -26,12 +28,22 @@ const options = {
     },
     servers: [
       {
-        url: "https://first-server-jmlo.onrender.com/",
+        url: "http://localhost:3000/",
       },
     ],
   },
-  apis: [`${__dirname}/routes/*.js`], // Correct the path to your routes
+  apis: [`${__dirname, "routes", "*.js"}`], // Correct the path to your routes
 };
+/**
+ * @swagger
+ * /categories:
+ *   get:
+ *     name: Retrieve a list of categories
+ *     description: This endpoint returns a list of all categories.
+ *     responses:
+ *       200:
+ *         description: A list of categories
+ */
 
 const specs = swaggerJSDoc(options);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
