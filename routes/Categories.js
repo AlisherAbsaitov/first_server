e213a9d82e30cf7fe457b39e05f5a8736020f68c
AlisherAbsaitov <alisher24032002@gmail.com>
2024-09-26 13:40:04 +0500

    add potect2

--- routes/Categories.js
+++ routes/Categories.js
@@ -1,16 +1,17 @@
 import express from "express";
 import * as categoriesController from "../controllers/categoriesControllers.js";
+import { protect } from "../controllers/authController.js";
 const router = express.Router();
 
 router
   .route("/")
   .get(categoriesController.getAllCategories)
-  .post(categoriesController.createCategory);
+  .post(protect,categoriesController.createCategory);
 
 router
   .route("/:id")
-  .delete(categoriesController.deleteCategory)
-  .patch(categoriesController.updateCategory)
+  .delete(protect,categoriesController.deleteCategory)
+  .patch(protect,categoriesController.updateCategory)
   .get(categoriesController.getCategory);
 
 export default router;
