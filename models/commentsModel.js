2bf5e9c1fc430103d135902b539f772bf9a1e5a9
AlisherAbsaitov <alisher24032002@gmail.com>
2024-09-27 21:19:20 +0500

    add productID

--- models/commentsModel.js
+++ models/commentsModel.js
@@ -6,23 +6,10 @@ const commentsSchema = new mongoose.Schema({
     typeof: "string",
   },
   productId: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Types.ObjectId,
     required: true,
     ref: "products",
   },
-});
-
-commentsSchema.pre("save", async function (next) {
-  if (this.isModified("productId")) {
-    // Fetch the category document using the id
-    const product = await Products.findById(this.productId);
-    if (!product) {
-      return next(new Error("Categoriya topilmadi"));
-    }
-    // Set the category name
-    this.product = product.name;
-  }
-  next();
 });
 
 export const Comments = mongoose.model("Comments",commentsSchema);
